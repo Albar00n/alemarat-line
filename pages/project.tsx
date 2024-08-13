c0443c6a5894c1394b763c2a7317dda8575acf90
Albar00n <>
2024-08-13 09:04:05 +0400

    a

--- pages/project.tsx
+++ pages/project.tsx
@@ -1,26 +1,36 @@
-import { GetStaticProps } from 'next'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import { useTranslation } from 'react-i18next'
-import Seo from '@/components/Seo'
-import ProjectDetails from '@/components/PagesDetails/ProjectDetails'
+// import { GetStaticProps } from 'next'
+// import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+// import { useTranslation } from 'react-i18next'
+// import Seo from '@/components/Seo'
+// import ProjectDetails from '@/components/PagesDetails/ProjectDetails'
 
-const Project = () => {
-  const { t } = useTranslation('common')
+// const Project = () => {
+//   const { t } = useTranslation('common')
 
+//   return (
+//     <>
+//       <Seo title="Project" url="/projects" />
+
+//       <ProjectDetails />
+//     </>
+//   )
+// }
+// export const getStaticProps: GetStaticProps = async ({ locale }) => {
+//   return {
+//     props: {
+//       ...(await serverSideTranslations(locale || '', ['common'])),
+//     },
+//     // revalidate: 20,
+//   }
+// }
+// export default Project
+
+import React from 'react'
+
+const HomeW = () => {
   return (
-    <>
-      <Seo title="Project" url="/projects" />
-
-      <ProjectDetails />
-    </>
+    <div>HomeW</div>
   )
 }
-export const getStaticProps: GetStaticProps = async ({ locale }) => {
-  return {
-    props: {
-      ...(await serverSideTranslations(locale || '', ['common'])),
-    },
-    // revalidate: 20,
-  }
-}
-export default Project
+
+export default HomeW
--- pages/services.tsx
+++ pages/services.tsx
@@ -1,26 +1,36 @@
-import { GetStaticProps } from 'next'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import { useTranslation } from 'react-i18next'
-import Seo from '@/components/Seo'
-import ServicesDetails from '@/components/PagesDetails/ServicesDetails'
+// import { GetStaticProps } from 'next'
+// import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+// import { useTranslation } from 'react-i18next'
+// import Seo from '@/components/Seo'
+// import ServicesDetails from '@/components/PagesDetails/ServicesDetails'
 
-const Services = () => {
-  const { t } = useTranslation('common')
+// const Services = () => {
+//   const { t } = useTranslation('common')
 
+//   return (
+//     <>
+//       <Seo title="Services" url="/services" />
+//       <ServicesDetails />
+//     </>
+//   )
+// }
+
+// export const getStaticProps: GetStaticProps = async ({ locale }) => {
+//   return {
+//     props: {
+//       ...(await serverSideTranslations(locale || '', ['common'])),
+//     },
+//     // revalidate: 20,
+//   }
+// }
+// export default Services
+
+import React from 'react'
+
+const HomeW = () => {
   return (
-    <>
-      <Seo title="Services" url="/services" />
-      <ServicesDetails />
-    </>
+    <div>HomeW</div>
   )
 }
 
-export const getStaticProps: GetStaticProps = async ({ locale }) => {
-  return {
-    props: {
-      ...(await serverSideTranslations(locale || '', ['common'])),
-    },
-    // revalidate: 20,
-  }
-}
-export default Services
+export default HomeW
